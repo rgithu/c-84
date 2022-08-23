1e44f2e530241522d6bb35ccc3ea639f81aedf09
rgithu <92245617+rgithu@users.noreply.github.com>
2022-08-23 17:01:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,14 +10,14 @@ img_x = 100;
 img_y = 100;
 
 function add() {
-	img_imgTag = new Image(); //defining a variable with a new image
-	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
-	img_imgTag.src = img_image;   // load image
+	image_imgTag = new Image(); //defining a variable with a new image
+	image_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	image_imgTag.src = img_image;   // load image
 }
 
 function uploadimg() {
 
-	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+	ctx.drawImage(image_imgTag, img_x, img_y, img_width, img_height);
 }
 
 //Write a code to grab the key-pressed event
@@ -32,10 +32,7 @@ function my_keydown(e)
 		aplhabetkey();
 		document.getElementById("d1").innerHTML="You Pressed Alphabet key";
 		console.log(aplhabetkey)
-	    else{
-		otherkey();
-		document.getElementById("d1").innerHTML="You pressed symbol or other key";
-	}
+	   
 }
 
 function aplhabetkey()
@@ -45,14 +42,18 @@ function aplhabetkey()
 }
 function numberkey()
 {
-	
+	img_image="numkey.png";
+	add();	
 }
 function arrowkey()
 {
+	img_image="Arrkey.png";
+	add();
 }
 function specialkey()
 {
-	
+	img_image="spkey.png";
+	add();
 }
 function otherkey()
 {
